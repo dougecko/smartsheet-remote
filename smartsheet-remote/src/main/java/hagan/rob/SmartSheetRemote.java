5296612b66e991baa1ef7d657ccb76d7609373c0
Doug <doug@stocksoftware.com.au>
2016-06-29 20:26:26 +1000

    Initial commit.

---
+++ smartsheet-remote/src/main/java/hagan/rob/SmartSheetRemote.java
@@ -0,0 +1,9 @@
+package hagan.rob;
+
+public class SmartSheetRemote
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
---
+++ smartsheet-remote/src/test/java/hagan/rob/SmartSheetRemoteTest.java
@@ -0,0 +1,38 @@
+package hagan.rob;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class SmartSheetRemoteTest
+        extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public SmartSheetRemoteTest(String testName)
+    {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite(SmartSheetRemoteTest.class);
+    }
+
+    /**
+     * Rigorous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue(true);
+    }
+}
